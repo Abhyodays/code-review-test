d8b718dd9517fde4f61921e120ef8b0b381edb10
Abhyodays <abhyodays@gmail.com>
2024-01-24 10:20:22 +0530

    repair code error

--- DAL/Repositories/BookRepository.cs
+++ DAL/Repositories/BookRepository.cs
@@ -65,10 +65,10 @@ namespace DAL.Repositories
 
         public Book GetById(int id)
         {
-            // try
-            // {
-            //     return _context.Books.Include(book => book.LentByUser).Include(book => book.BorrowedByUser).FirstOrDefault(b => b.Id == id);
-            // }
+            try
+            {
+                return _context.Books.Include(book => book.LentByUser).Include(book => book.BorrowedByUser).FirstOrDefault(b => b.Id == id);
+            }
             catch(Exception ex)
             {
                 _logger.LogError(ex, "Error occurred while finding a book in the database.");
